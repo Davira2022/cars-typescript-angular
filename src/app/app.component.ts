73bba1b1a86ec20a6fa84f77f2e1443d0303f2c3
Davira2022 <112394917+Davira2022@users.noreply.github.com>
2022-12-11 12:28:16 +0200

    message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, HostListener} from '@angular/core';
 import {FormBuilder, Validators} from "@angular/forms";
+import {AppService} from "./app.service";
 
 @Component({
   selector: 'app-root',
@@ -13,53 +14,82 @@ export class AppComponent {
     car: ['', Validators.required]
   })
 
-  carsData = [
-    {
-      image: "1.png",
-      name: "Lamborghini Huracan Spyder",
-      gear: "полный",
-      engine: 5.2,
-      places: 2
-    },
-    {
-      image: "2.png",
-      name: "Chevrolet Corvette",
-      gear: "полный",
-      engine: 6.2,
-      places: 2
-    },
-    {
-      image: "3.png",
-      name: "Ferrari California",
-      gear: "полный",
-      engine: 3.9,
-      places: 4
-    },
-    {
-      image: "4.png",
-      name: "Lamborghini Urus",
-      gear: "полный",
-      engine: 4.0,
-      places: 5
-    },
-    {
-      image: "5.png",
-      name: "Audi R8",
-      gear: "полный",
-      engine: 5.2,
-      places: 2
-    },
-    {
-      image: "6.png",
-      name: "Chevrolet Camaro",
-      gear: "полный",
-      engine: 2.0,
-      places: 4
-    },
-  ];
+  // carsData = [
+  //   {
+  //     image: "1.png",
+  //     name: "Lamborghini Huracan Spyder",
+  //     gear: "полный",
+  //     engine: 5.2,
+  //     places: 2
+  //   },
+  //   {
+  //     image: "2.png",
+  //     name: "Chevrolet Corvette",
+  //     gear: "полный",
+  //     engine: 6.2,
+  //     places: 2
+  //   },
+  //   {
+  //     image: "3.png",
+  //     name: "Ferrari California",
+  //     gear: "полный",
+  //     engine: 3.9,
+  //     places: 4
+  //   },
+  //   {
+  //     image: "4.png",
+  //     name: "Lamborghini Urus",
+  //     gear: "полный",
+  //     engine: 4.0,
+  //     places: 5
+  //   },
+  //   {
+  //     image: "5.png",
+  //     name: "Audi R8",
+  //     gear: "полный",
+  //     engine: 5.2,
+  //     places: 2
+  //   },
+  //   {
+  //     image: "6.png",
+  //     name: "Chevrolet Camaro",
+  //     gear: "полный",
+  //     engine: 2.0,
+  //     places: 4
+  //   },
+  // ];
 
-  constructor(private fb: FormBuilder) {
+  carsData: any;
+
+  constructor(private fb: FormBuilder, private appService: AppService) {
   }
+
+  ngOnInit() {
+    this.appService.getData(this.category).subscribe(carsData => this.carsData = carsData);
+  }
+
+
+
+  trans: any;
+
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(e: MouseEvent) {
+    this.trans = {transform: 'translate3d(' + ((e.clientX * 0.3) / 8) + 'px,' + ((e.clientY * 0.3) / 8) + 'px,0px)'};
+  }
+
+  bgPos: any;
+
+  @HostListener('document:scroll', ['$event'])
+  onScroll() {
+    this.bgPos = {backgroundPositionX: '0' + (0.3 * window.scrollY) + 'px'};
+  }
+
+  category: string = 'sport';
+  toggleCategory(category: string) {
+    this.category = category;
+    this.ngOnInit();
+  }
+
   goScroll(target: HTMLElement, car?: any) {
     target.scrollIntoView({behavior: "smooth"});
     if (car) {
@@ -67,22 +97,22 @@ export class AppComponent {
     }
   }
 
-  trans: any;
-  @HostListener('document:mousemove', ['$event'])
-  onMouseMove(e: MouseEvent) {
-    this.trans = {transform: 'translate3d(' + ((e.clientX * 0.3) / 8) + 'px,' + ((e.clientY * 0.3) / 8) + 'px,0px)'};
-  }
-
-  bgPos: any;
-  @HostListener('document:scroll', ['$event'])
-  onScroll() {
-    this.bgPos = {backgroundPositionX: '0' + (0.3 * window.scrollY) + 'px'};
-  }
-
   onSubmit() {
     if (this.priceForm.valid) {
-      alert("Спасибо за заявку! Мы свяжемся с Вами в ближайшее время!");
-      this.priceForm.reset();
+
+      this.appService.sendQuery(this.priceForm.value)
+        .subscribe(
+          // result => console.log(result)
+          {
+            next: (response: any) => {
+              alert(response.message);
+              this.priceForm.reset();
+            },
+            error: (response) => {
+              alert(response.error.message)
+            }
+          }
+        );
     }
   }
 }
